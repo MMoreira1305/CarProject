ce16cff07f8ff0d1146192509e88f0a1b260b276
Hugo Vinícius <ugopessoa@gmail.com>
2023-11-06 14:33:51 -0300

    add method post in user

--- backend/src/main/java/com/carproject/backend/controller/UserController.java
+++ backend/src/main/java/com/carproject/backend/controller/UserController.java
@@ -1,16 +1,30 @@
 package com.carproject.backend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.carproject.backend.model.User;
+import com.carproject.backend.serv.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @GetMapping("/{username}")
-    public String getUser(@PathVariable String username){
+    public String getUser(@PathVariable String username) {
         return username;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity setUser(@RequestBody User user) {
+        var userDTO = new UserService().post(user);
+        return ResponseEntity.ok(userDTO);
     }
 }
--- backend/src/main/java/com/carproject/backend/serv/UserService.java
+++ backend/src/main/java/com/carproject/backend/serv/UserService.java
@@ -8,7 +8,11 @@ import com.carproject.backend.repo.BrandRepository;
 import com.carproject.backend.repo.CarRepository;
 import com.carproject.backend.repo.CategoryRepository;
 import com.carproject.backend.repo.UserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -23,11 +27,11 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public List<UserDTO> getAll(){
-        List<User> users =  userRepository.findAll();
+    public List<UserDTO> getAll() {
+        List<User> users = userRepository.findAll();
         List<UserDTO> usersDTO = new ArrayList<>();
 
-        for (User user : users){
+        for (User user : users) {
             UserDTO userDTO = new UserDTO();
             userDTO.createFor(user);
             usersDTO.add(userDTO);
@@ -35,17 +39,31 @@ public class UserService {
         return usersDTO;
     }
 
-    public UserDTO post(User user){
+    public ResponseEntity verifyUser(User user) {
+        var verifyUser = this.userRepository.findByLogin(user.getLogin());
+
+        if (verifyUser != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: User Already exists!!");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("User registered successfully");
+    }
+
+    public UserDTO post(User user) {
+        verifyUser(user);
+        var passwordHashred = BCrypt.withDefaults()
+                .hashToString(12, user.getPassword().toCharArray());
+
+        user.setPassword(passwordHashred);
         userRepository.save(user);
         UserDTO userDTO = new UserDTO(user);
         return userDTO;
     }
 
-    public ResponseEntity getById(Long id){
+    public ResponseEntity getById(Long id) {
         Assert.notNull(id, "Não foi encontrar pois está sem o ID");
         Optional<User> user = userRepository.findById(id);
 
-        if(user.isPresent()){
+        if (user.isPresent()) {
             UserDTO userDTO = new UserDTO(user.get());
             return ResponseEntity.ok(userDTO);
         }
@@ -57,7 +75,7 @@ public class UserService {
         Assert.notNull(login, "Não foi possível atualizar o registro pois não foi passado a placa");
         Optional<User> user = userRepository.findByLogin(login);
 
-        if(user.isPresent()){
+        if (user.isPresent()) {
             UserDTO userDTO = new UserDTO(user.get());
             return ResponseEntity.ok(userDTO);
         }
@@ -68,9 +86,9 @@ public class UserService {
         Assert.notNull(name, "Não foi possível atualizar o registro pois não foi passado o nome");
         List<User> users = userRepository.findByName(name);
 
-        if(!users.isEmpty()){
-            if(users.size() < 2){
-                for (User user : users){
+        if (!users.isEmpty()) {
+            if (users.size() < 2) {
+                for (User user : users) {
                     UserDTO userDTO = new UserDTO(user);
                     return ResponseEntity.ok(userDTO);
                 }
@@ -78,7 +96,7 @@ public class UserService {
 
             List<UserDTO> usersDTO = new ArrayList<>();
 
-            for (User user : users){
+            for (User user : users) {
                 UserDTO userDTO = new UserDTO(user);
                 usersDTO.add(userDTO);
             }
@@ -87,24 +105,23 @@ public class UserService {
         return ResponseEntity.notFound().build();
     }
 
-    public UserDTO updateUser(Long id, User user){
+    public UserDTO updateUser(Long id, User user) {
         Assert.notNull(id, "Não foi possível atualizar o registro");
 
         Optional<User> userOptional = userRepository.findById(id);
 
-        if (userOptional.isPresent()){
+        if (userOptional.isPresent()) {
             user.setId(userOptional.get().getId());
             post(user);
             UserDTO userDTO = new UserDTO(user);
 
             return userDTO;
-        }
-        else {
+        } else {
             throw new RuntimeException("Não foi possível localizar o carro no DBA");
         }
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         Assert.notNull(id, "Não foi encontrar pois está sem o ID");
         userRepository.deleteById(id);
     }
